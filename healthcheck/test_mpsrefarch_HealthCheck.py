eb2b06950714afdf122c535fbbc85262689d4281
PRADEEP VASUDEVAN <63336986+pvasudev-mw@users.noreply.github.com>
2021-08-05 16:31:22 -0400

    healthcheck support for R2021a

--- healthcheck/test_mpsrefarch_HealthCheck.py
+++ healthcheck/test_mpsrefarch_HealthCheck.py
@@ -12,7 +12,7 @@ import cloud_deployment_testtools.deploy as DeployOp
 from datetime import date
 import datetime
 
-def main(tenant_id_arg, client_id_arg, client_secret_arg, subscription_id_arg, username, password, ipAddress):
+def main(tenant_id_arg, client_id_arg, client_secret_arg, subscription_id_arg, username, password, ipAddress, keyVaultSecret, ManagedIdentityResourceID):
     # Reference architectures in production.
     ref_arch_name = 'matlab-production-server-on-azure'
 
@@ -23,13 +23,22 @@ def main(tenant_id_arg, client_id_arg, client_secret_arg, subscription_id_arg, u
     credentials = AzureAuth.authenticate_client_key(tenant_id, client_id, client_secret)
     subscription_id = subscription_id_arg
 
-    parameters = {
+    parameters1 = {
         "adminUsername": username,
         "adminPassword": password,
         "Allow connections from": ipAddress,
         "Platform": "Linux"
     }
 
+    parameters2 = {
+        "adminUsername": username,
+        "adminPassword": password,
+        "Allow connections from": ipAddress,
+        "Platform": "Linux",
+        "KeyVaultCertificateSecretID": keyVaultSecret,
+        "ManagedIdentityResourceIDForKeyVault": ManagedIdentityResourceID
+    }
+        
     location = 'eastus'
     
 
@@ -50,13 +59,17 @@ def main(tenant_id_arg, client_id_arg, client_secret_arg, subscription_id_arg, u
         print("Date time before deployment of stack:-", ct)
 
         try:
-            DeployOp.deploy_production_template(credentials, subscription_id, resource_group_name, location, ref_arch_name, template_name, parameters)
+            if matlab_release == "R2020b":
+                DeployOp.deploy_production_template(credentials, subscription_id, resource_group_name, location, ref_arch_name, template_name, parameters1)
+            else:
+                DeployOp.deploy_production_template(credentials, subscription_id, resource_group_name, location, ref_arch_name, template_name, parameters2)  
         except Exception as e:
             raise (e)
+        
         # delete the deployment
         DeployOp.delete_resourcegroup(credentials, subscription_id, resource_group_name)
         ct = datetime.datetime.now()
         print("Date time after deployment and deletion of stack:-", ct)
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9])
