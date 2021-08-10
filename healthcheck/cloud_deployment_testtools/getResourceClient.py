9226b0b2d6302382a09d6227ee92f0e2aed54398
PRADEEP VASUDEVAN <63336986+pvasudev-mw@users.noreply.github.com>
2021-08-10 00:49:00 -0400

    Update getResourceClient.py

--- healthcheck/cloud_deployment_testtools/getResourceClient.py
+++ healthcheck/cloud_deployment_testtools/getResourceClient.py
@@ -1,5 +1,5 @@
 from azure.mgmt.resource import ResourceManagementClient
 
 def get_resource_client(credentials, subscription_id) : 
-    resource_client = ResourceManagementClient(credentials, subscription_id, "2019-05-01")
+    resource_client = ResourceManagementClient(credentials, subscription_id)
     return resource_client
